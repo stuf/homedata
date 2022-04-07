fb0f6258dbfba6c4037fc9eb19cbc11973092fa9
Stefan Rimaila <stefan@rimaila.fi>
2022-04-07 14:13:19 +0300

    Start work on moving things over to CLI

---
+++ index.js
@@ -0,0 +1,64 @@
+#!/usr/bin/env node
+require('./cli').main();
+
+// const L = require('partial.lenses');
+// const { fromEvents, stream, fromPromise, constant } = require('kefir');
+// const mqtt = require('mqtt');
+// const { Sequelize, DataTypes } = require('sequelize');
+// const location = require('./models/location');
+// const measurement = require('./models/measurement');
+// const config = require('./config/config.json');
+
+// const cli = require('./cli');
+
+// const sequelize = new Sequelize(config.development);
+
+// const Measurement = measurement(sequelize, DataTypes);
+
+// const client = mqtt.connect({ host: mqttHost, port: mqttPort });
+
+// const connected$ = fromEvents(client, 'connect');
+
+// const messages$ = connected$
+//   .flatMap(() =>
+//     stream(emitter => {
+//       client.subscribe('etm12/sensor/ruuvitag/#').on('message', (t, m, p) => {
+//         const d = JSON.parse(m);
+
+//         const dʼ = L.transform(
+//           L.seq(
+//             [
+//               L.props('temperature', 'humidity'),
+//               L.values,
+//               L.modifyOp(v => parseFloat(v.toFixed(6))),
+//             ],
+//             ['source', L.setOp(d.mac)],
+//             ['time', L.modifyOp(() => new Date())],
+//             ['mac', L.removeOp],
+//           ),
+//           d,
+//         );
+
+//         emitter.emit({
+//           topic: t,
+//           json: dʼ,
+//         });
+//       });
+//     }),
+//   )
+//   .toProperty();
+
+// //
+
+// const grouped$ = messages$.scan(
+//   (prev, next) => L.set(next.json.source, next.json, prev),
+//   {},
+// );
+
+// const insert$ = grouped$.throttle(10000).flatMap(o => {
+//   const xs = Object.values(o);
+
+//   return fromPromise(Measurement.bulkCreate(xs));
+// });
+
+// insert$.onValue(() => {});
